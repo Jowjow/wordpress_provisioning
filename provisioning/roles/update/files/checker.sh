1c411440fe5cc9724dd627c226febc832c62ce77
Jowjow <araujowjow@gmail.com>
2020-05-20 17:10:25 -0300

    Adding cicd provisioning and setup and update separation

--- provisioning/roles/update/files/checker.sh
+++ provisioning/roles/update/files/checker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ $(curl -sL -w "%{http_code}\\n" "https://localhost:80/version.txt" -o /dev/null)  != "200" ]; then
-    echo "Wordpress is not healthy1"; exit 1;
+if [ $(curl -sL -w "%{http_code}\\n" "https://devopstest.gigmedia.tech" -o /dev/null)  != "200" ]; then
+    echo "Wordpress is not healthy: Non-200 response"; exit 1;
 fi
 
-if [ $(curl "https://localhost:80/version.txt")  != "1.0.0" ]; then
-    echo "Wordpress is not healthy2"; exit 1;
+if [ $(curl "https://devopstest.gigmedia.tech/version.txt")  != "1.0.0" ]; then
+    echo "Wordpress is not healthy: version.txt does not match"; exit 1;
 fi
 
 exit 0
